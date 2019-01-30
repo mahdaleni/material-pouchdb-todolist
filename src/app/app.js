eedb7ac6523802d06a7eef1553d25b24fc3f8c2f
Arif <dewisandrax@gmail.com>
2019-01-30 15:09:14 +0700

    Update app.js

--- src/app/app.js
+++ src/app/app.js
@@ -15,7 +15,7 @@
         .config(AppConfig)
         .constant('cfg', {
             'pouchDBName': 'tasks',
-            'pouchDBRemote': 'https://couchzz.netlify.com/',
+            'pouchDBRemote': 'https://couchdb-258122.smileupps.com/',
             // 'pouchDBRemote': 'http://localhost:5984/'
         });
 
